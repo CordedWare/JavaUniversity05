44f0a87b5f87183aa379a137438d8f37130070e0
daev93@mail.ru <daev93@mail.com>
2022-10-10 17:44:03 +0500

    hw7 new

---
+++ src/homework/HW7.java
@@ -0,0 +1,127 @@
+package homework;
+
+public class HW7 {
+
+    static int tcNumber = 1; // numbering variable for tasks
+
+    public static void printTCNumber() {
+        System.out.println("\n\033[1;32m" + "Task_" + tcNumber++ + "\u001B[0m");
+    } // numbering method of tasks
+
+    public static void printTestResult(String value) {
+        System.out.println(value);
+    } // report printing method for AAA
+
+    public static String verifyEquals(int exceptedResult, int actualResult) {
+        if (exceptedResult == actualResult) {
+
+            return "\033[1;32m" + "Pass" + "\u001B[0m";
+        } else {
+
+            return "\033[1;31m" + "Fail" + "\u001B[0m";
+        }
+    } // "int" method to compare 'expected' and 'actual' result
+
+    public static String verifyEquals(String exceptedResult, String actualResult) {
+        if (exceptedResult == actualResult) {
+
+            return "\033[1;32m" + "Pass" + "\u001B[0m";
+        } else {
+
+            return "\033[1;31m" + "Fail" + "\u001B[0m";
+        }
+    } // "String" method to compare 'expected' and 'actual' result
+
+    /// tasks method ///
+
+    public static String switchOfMonth1_1(int month) {
+        month = month;
+        String monthString;
+
+        switch (month) {
+            case 1:
+                monthString = "Janiary";
+                break;
+            case 2:
+                monthString = "February";
+                break;
+            case 3:
+                monthString = "Marth";
+                break;
+            case 4:
+                monthString = "April";
+                break;
+            case 5:
+                monthString = "May";
+                break;
+            case 6:
+                monthString = "June";
+                break;
+            case 7:
+                monthString = "July";
+                break;
+            case 8:
+                monthString = "August";
+                break;
+            case 9:
+                monthString = "September";
+                break;
+            case 10:
+                monthString = "October";
+                break;
+            case 11:
+                monthString = "November";
+                break;
+            case 12:
+                monthString = "December";
+                break;
+            default:
+                monthString = "Invalid month";
+                break;
+        }
+        return monthString;
+    } // Task 1
+
+    public static void main(String[] args) {
+
+        // Task_1_1
+        // Написать метод с оператором switch, который печатает месяц, когда выбираем число.
+        // Протестировать на граничные значения.
+
+        // AAA - triple "A" правило создания автоматизированного тест кейса
+
+        // A = arrange (объявить все данные, которые нам известны)
+        int month = 15;
+        String exceptedResult = "Invalid month";
+
+        // A = act (вызов метода, действие для получения actual result)
+        String actualResult = switchOfMonth1_1(month);
+
+        // A = assert (сравнение excepted result c actual result и вывод, прошел тест или упал)
+        verifyEquals(exceptedResult,actualResult);
+
+        // Report Task_1 (печать результата)
+        printTestResult(verifyEquals(exceptedResult,actualResult));
+        System.out.println(switchOfMonth1_1(month));
+
+
+        {
+        // Task_1
+        //
+        // Протестировать на граничные значения.
+
+        // AAA - triple "A" правило создания автоматизированного тест кейса
+
+        // A = arrange (объявить все данные, которые нам известны)
+
+        // A = act (вызов метода, действие для получения actual result)
+
+        // A = assert (сравнение excepted result c actual result и вывод, прошел тест или упал)
+
+        // Report Task_1 (печать результата)
+    }
+
+
+
+    }
+}
